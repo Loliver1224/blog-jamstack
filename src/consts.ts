1d2caee563162601d1e1ba9ec1d4fb55a03aac8e
Daichi Furukawa <k@u9k.jp>
2026-06-27 03:34:29 +0900

    サイトタイトル変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Loliver's Landscape"
-export const SITE_DESCRIPTION = "Welcome to my website!"
+export const SITE_TITLE = "Cybernaut's Landscape"
+export const SITE_DESCRIPTION = "Welcome to my tech blog!"
